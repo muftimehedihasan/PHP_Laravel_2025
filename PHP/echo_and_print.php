4564ce3280683c703a1bec58c550d29c1b324e4a
muftimehedihasan <mehedihasan.projects@gmail.com>
2025-01-06 21:12:09 +0600

    echo and print Statements

---
+++ PHP/echo_and_print.php
@@ -0,0 +1,3 @@
+<!-- With PHP, there are two basic ways to get output: echo and print.
+
+In this tutorial we use echo or print in almost every example. So, this chapter contains a little more info about those two output statements. -->
