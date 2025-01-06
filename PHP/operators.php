828872631d4914915aad3f5a90cfeef1c7b28098
muftimehedihasan <mehedihasan.projects@gmail.com>
2025-01-07 00:27:07 +0600

    operators

---
+++ PHP/operators.php
@@ -0,0 +1,15 @@
+<!-- PHP Operators
+
+Operators are used to perform operations on variables and values.
+
+PHP divides the operators in the following groups:
+
+    Arithmetic operators
+    Assignment operators
+    Comparison operators
+    Increment/Decrement operators
+    Logical operators
+    String operators
+    Array operators
+    Conditional assignment operators
+ -->
