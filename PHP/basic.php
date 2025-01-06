34ee6ebac7d28200983d180b4f724dd0710e9064
muftimehedihasan <mehedihasan.projects@gmail.com>
2025-01-06 20:54:01 +0600

    basic

---
+++ PHP/basic.php
@@ -0,0 +1,29 @@
+<!-- Basic PHP Syntax -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "Hello World;" ?>
+</body>
+</html>
+
+
+
+<?php 
+echo"Hello World";
+
+
+
+// Comments in the Middle of the Code
+
+// The multi-line comment syntax can also be used to prevent execution of parts inside a code-line:
+
+$x = 5 /* + 15 */ + 5;
+echo $x;
+$x1= 5 /* + 15 */ +5;
+
