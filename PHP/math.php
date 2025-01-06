7ad014f2661f855940565fc3e40ad4ecf42928fd
muftimehedihasan <mehedihasan.projects@gmail.com>
2025-01-06 22:21:28 +0600

    math

---
+++ PHP/math.php
@@ -0,0 +1,7 @@
+<!-- HP has a set of math functions that allows you to perform mathematical tasks on numbers.
+PHP pi() Function
+
+The pi() function returns the value of PI:
+Example
+
+echo(pi()); -->
