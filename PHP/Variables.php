947764d9eefead499f3c869d0d026d67f19074cd
muftimehedihasan <mehedihasan.projects@gmail.com>
2025-01-06 21:09:18 +0600

    Variables

--- PHP/Variables.php
+++ PHP/Variables.php
@@ -31,3 +31,18 @@ The var_dump() function returns the data type and the value: -->
 <?php
 $x = 5;
 var_dump($x);
+
+
+// PHP Variables Scope
+
+// In PHP, variables can be declared anywhere in the script.
+
+// The scope of a variable is the part of the script where the variable can be referenced/used.
+
+// PHP has three different variable scopes:
+
+//     local
+//     global
+//     static
+
+
