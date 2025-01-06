43cab50d1cc73e4bc28eb42073e25975f67a6bab
muftimehedihasan <mehedihasan.projects@gmail.com>
2025-01-06 22:11:44 +0600

    escape characters

---
+++ PHP/escape_characters.php
@@ -0,0 +1,23 @@
+<!-- Escape Character
+
+To insert characters that are illegal in a string, use an escape character.
+
+An escape character is a backslash \ followed by the character you want to insert.
+
+An example of an illegal character is a double quote inside a string that is surrounded by double quotes: -->
+
+
+<!-- 
+Escape Characters
+
+Other escape characters used in PHP:
+Code 	Result 	Try it
+\' 	Single Quote 	
+\" 	Double Quote 	
+\$ 	PHP variables 	
+\n 	New Line 	
+\r 	Carriage Return 	
+\t 	Tab 	
+\f 	Form Feed 	
+\ooo 	Octal value 	
+\xhh 	Hex value -->
