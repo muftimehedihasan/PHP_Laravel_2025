8c472484f17f70c703a501dea1bd90c2737d23e7
muftimehedihasan <mehedihasan.projects@gmail.com>
2025-01-06 21:04:09 +0600

    variables

--- PHP/Variables.php
+++ PHP/Variables.php
@@ -7,3 +7,27 @@ echo $x;
 echo "<br>";
 echo $y;
 ?>
+
+<!-- PHP supports the following data types:
+
+    String
+    Integer
+    Float (floating point numbers - also called double)
+    Boolean
+    Array
+    Object
+    NULL
+    Resource -->
+
+<!-- 
+    Get the Type
+
+To get the data type of a variable, use the var_dump() function. -->
+
+<!-- Example
+
+The var_dump() function returns the data type and the value: -->
+
+<?php
+$x = 5;
+var_dump($x);
